8e34a329e2da701f707bbb84913c0e46303018a4
Efito Solutions Pvt Ltd <efito@Efitos-Mac-mini.local>
2024-03-05 21:33:07 +0530

    first commit

---
+++ lib/custom_bootom_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomBar extends StatefulWidget {
+  const CustomBottomBar({super.key});
+
+  @override
+  State<CustomBottomBar> createState() => _CustomBottomBarState();
+}
+
+class _CustomBottomBarState extends State<CustomBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
