bbbffefa3f5ed17723d8d8dec85452ec368396ba
Efito Solutions Pvt Ltd <efito@Efitos-Mac-mini.local>
2024-03-05 23:12:23 +0530

    changes

--- lib/custom_bootom_bar.dart
+++ lib/custom_bootom_bar.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/pages/chat.dart';
+import 'package:test_app/pages/dashboard.dart';
+import 'package:test_app/pages/notification.dart';
+import 'package:test_app/pages/profile.dart';
 
 class CustomBottomBar extends StatefulWidget {
   const CustomBottomBar({super.key});
@@ -8,8 +12,152 @@ class CustomBottomBar extends StatefulWidget {
 }
 
 class _CustomBottomBarState extends State<CustomBottomBar> {
+  int currentTab = 0;
+  final List<Widget> screens = [
+    Dashboard(),
+    Chat(),
+    NotificationView(),
+    Profile()
+  ];
+
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = Dashboard();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: PageStorage(
+        bucket: bucket,
+        child: currentScreen,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Dashboard();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.dashboard,
+                            color: currentTab == 0 ? Colors.blue : Colors.grey,
+                          ),
+                          Text(
+                            'Dashboard',
+                            style: TextStyle(
+                                color: currentTab == 0
+                                    ? Colors.blue
+                                    : Colors.grey),
+                          )
+                        ]),
+                  ),
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Chat();
+                        currentTab = 1;
+                      });
+                    },
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.dashboard,
+                            color: currentTab == 1 ? Colors.blue : Colors.grey,
+                          ),
+                          Text(
+                            'Chat',
+                            style: TextStyle(
+                                color: currentTab == 1
+                                    ? Colors.blue
+                                    : Colors.grey),
+                          )
+                        ]),
+                  ),
+                
+                  
+                 
+                ],
+              ),
+               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = NotificationView();
+                        currentTab = 2;
+                      });
+                    },
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.dashboard,
+                            color: currentTab == 2 ? Colors.blue : Colors.grey,
+                          ),
+                          Text(
+                            'Notification',
+                            style: TextStyle(
+                                color: currentTab == 2
+                                    ? Colors.blue
+                                    : Colors.grey),
+                          )
+                        ]),
+                  ),
+              
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Profile();
+                        currentTab = 3;
+                      });
+                    },
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.person,
+                            color: currentTab == 3 ? Colors.blue : Colors.grey,
+                          ),
+                          Text(
+                            'Profile',
+                            style: TextStyle(
+                                color: currentTab == 3
+                                    ? Colors.blue
+                                    : Colors.grey),
+                          )
+                        ]),
+                  ),
+                  
+                 
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
